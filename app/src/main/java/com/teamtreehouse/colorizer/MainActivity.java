5b2b77f3f40d074c7cc71fe2a2e2b438ac0b6ebf
wilfred <a@a.com>
2017-03-27 15:58:24 -0700

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/colorizer/MainActivity.java
@@ -0,0 +1,59 @@
+package com.teamtreehouse.colorizer;
+
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    int[] imageResIds = {R.drawable.cuba1, R.drawable.cuba2, R.drawable.cuba3};
+    int imageIndex = 0;
+    boolean color = true;
+    boolean red = true;
+    boolean green = true;
+    boolean blue = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        loadImage();
+    }
+
+    private void loadImage() {
+        Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+    }
+
+    private void updateSaturation() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        if (color) {
+            red = green = blue = true;
+            colorMatrix.setSaturation(1);
+        } else {
+            colorMatrix.setSaturation(0);
+        }
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+
+    private void updateColors() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        float[] matrix = {
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0,
+        };
+        if (!red) matrix[0] = 0;
+        if (!green) matrix[6] = 0;
+        if (!blue) matrix[12] = 0;
+        colorMatrix.set(matrix);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+}
